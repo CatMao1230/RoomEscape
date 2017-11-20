54a2c5482874006caed7ddf1d3836cd17ded6d29
CatMao1230 <cat841230@gmail.com>
2017-11-20 19:32:47 +0800

    Fix static resources cache.

--- app/server/__init__.py
+++ app/server/__init__.py
@@ -7,7 +7,7 @@
 
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_debugtoolbar import DebugToolbarExtension
 
 ################
@@ -58,3 +58,19 @@ def page_not_found(error):
 @app.errorhandler(500)
 def server_error_page(error):
     return render_template("errors/500.html"), 500
+
+######################
+#### static cache ####
+######################
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(os.path.dirname(app.root_path), "client", endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
