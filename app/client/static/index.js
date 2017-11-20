ef571906a6e3d2d571c8bef38d5850770186e70e
CatMao1230 <cat841230@gmail.com>
2017-11-20 21:50:35 +0800

    Change count down style.

--- app/client/static/index.js
+++ app/client/static/index.js
@@ -1,8 +1,23 @@
-$('#count-down').countdown('2018/04/30', function(event) {
-    var $this = $(this).html(event.strftime(''
-        + '%D Days '
-        + '%H Hours '
-        + '%M Minutes '
-        + '%S Seconds '
-    ));
-});
+let countDownHtml = (number, text) => {
+    return `
+    <div class='two wide column row'>
+      <h1 class='ui inverted header'>
+        ${number}
+        <div class='sub header'>
+          ${text}
+        </div>
+      </h1>
+    </div>
+    `
+}
+
+$('#count-down').countdown('2018/04/30', function (event) {
+    let $this = $(this).html(event.strftime(`
+        <div class='ui center aligned grid'>
+          ${countDownHtml('%D', 'Days')}
+          ${countDownHtml('%H', 'Hours')}
+          ${countDownHtml('%M', 'Minutes')}
+          ${countDownHtml('%S', 'Seconds')}
+        </div>`
+    ))
+})
